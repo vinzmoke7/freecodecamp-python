760378ec028fe3d1a46c978c8c4ea0f031605636
Ogungbola Kolade <nifemijohnson961@gmail.com>
2026-02-11 20:37:07 +0100

    travel plan commit

--- python display/travel-plan.py
+++ python display/travel-plan.py
@@ -3,31 +3,12 @@ is_raining = True
 has_bike = False
 has_car = True
 has_ride_share_app = True
-if has_car or has_ride_share_app:
-    if is_raining:
-        print ('user took a car or booked a ride')
-    else:
-        print ('user took a car or a ride by choice')
-elif has_bike and not is_raining:
-    print('user took his bike')
-if not distance_mi:
-    print (False)
-if distance_mi < 5:
-    print('short distance covered')
-elif distance_mi >=5 and distance_mi <=15:
-    print('medium distance covered')
-elif distance_mi >=15 and distance_mi <=19 :
-    print('long distance covered')
+
+if distance_mi <= 1:
+    print(not is_raining)
+
+elif distance_mi <= 6:
+    print(has_bike and not is_raining)
+
 else:
-    print('Maximum distance covered')
-if not is_raining:
-    distance_mi < 1
-    print(True)
-if has_bike and not is_raining:
-    distance_mi >1 and  distance_mi >= 6
-    print (True)
-if has_car or has_ride_share_app:
-    distance_mi > 6 
-    print(True)
-else:
-    print(False)
+    print(has_car or has_ride_share_app)
